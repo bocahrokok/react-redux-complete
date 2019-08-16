b82ee43747bd0ad6553e68263d1a02568813d2a8
bocahrokok <indieunistudy@gmail.com>
2019-08-16 22:58:53 +0700

    commit malam

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,6 +3,14 @@ import Ninjas from './Ninjas';
 
 
 function App() {
+  state = {
+    ninjas: [
+      {name: 'Ryu', age : 30, belt: 'black', id:1},
+      {name: 'Yoshi', age : 20, belt: 'green', id:2},
+      {name: 'Crystal', age : 25, belt: 'black', id:3}
+    ]
+  }
+
   return (
     <div className="App">
         <h1>My first react app!</h1>
