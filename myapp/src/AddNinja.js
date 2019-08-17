13e08c721174de3db9abac65f31910cab2f521b6
bocahrokok <indieunistudy@gmail.com>
2019-08-17 14:21:29 +0700

    commit form revisited

---
+++ myapp/src/AddNinja.js
@@ -0,0 +1,41 @@
+import React, { Component} from 'react'
+
+class AddNinja extends Component {
+    
+    state = {
+        name: null,
+        age: null,
+        belt: null
+    }
+
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value 
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
+    render( ){
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="name">Name: </label>
+                    <input type="text" name="" id="name" onChange={this.handleChange}/>
+                    <label htmlFor="name">age: </label>
+                    <input type="text" name="" id="age" onChange={this.handleChange}/>
+                    <label htmlFor="name">belt: </label>
+                   <input type="text" name="" id="belt" onChange={this.handleChange}/>
+
+                   <button>Submit</button>
+                </form> 
+            </div>
+        )
+    }
+}
+
+export default AddNinja
