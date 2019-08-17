f9581464284f17ee7a0dcf67b7039b05c260a83f
bocahrokok <indieunistudy@gmail.com>
2019-08-17 13:53:10 +0700

    commit conditional

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   state = {
     ninjas : [
       {name: 'Ryu', age: 30, belt: 'black', id:1},
-      {name: 'Ryu', age: 30, belt: 'black', id:1},
+      {name: '', age: 20, belt: 'black', id:1},
       {name: 'Ryu', age: 30, belt: 'black', id:1},
     ]
   }
