a00413de8b6e671dfb4bcd532dc585a24f580fcd
bocahrokok <indieunistudy@gmail.com>
2019-08-17 16:19:42 +0700

    commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -11,13 +11,21 @@ class App extends Component {
     ]
   }
 
+  addNinja = (ninja) => {
+      ninja.id = Math.random();
+      let ninjas = [...this.state.ninjas, ninja]
+      this.setState({
+        ninjas : ninjas
+      })
+  }
+
   render(){
     return (
       <div className="App">
         <h1>My first react app!</h1>
         <p>welcome</p>
         <Ninjas ninjas= {this.state.ninjas}/>
-        <AddNinja />
+        <AddNinja addNinja= {this.addNinja} />
 
       </div>
     );
